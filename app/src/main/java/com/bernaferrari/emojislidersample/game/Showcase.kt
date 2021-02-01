6b81a7cd3265307ed0a3170dd8076265026e9026
Gulzar Ahmed <gulzarhome54@gmail.com>
2021-02-01 14:48:10 +0530

    fix

--- app/src/main/java/com/bernaferrari/emojislidersample/game/Showcase.kt
+++ app/src/main/java/com/bernaferrari/emojislidersample/game/Showcase.kt
@@ -1,14 +1,21 @@
 package com.bernaferrari.emojislidersample.game
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.ValueCallback
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.bernaferrari.emojislidersample.R
+import com.bernaferrari.emojislidersample.game.util.bridge.BridgeHandler
+import kotlinx.android.synthetic.main.frag_main_game.*
 
 
 class Showcase : Fragment() {
+
+    private val TAG = "Showcase"
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -17,7 +24,17 @@ class Showcase : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        button3.setOnClickListener {
+            web_view.loadUrl("http://10.0.2.2:7456/")
+        }
 
+        web_view.setDefaultHandler { data, function ->
+            Log.d(TAG, "setDefaultHandler: $data")
+        }
+
+        web_view.registerHandler("openReactNativeStore") { data, function ->
+            Log.d(TAG, "setDefaultHandler: $data")
+        }
     }
 
 
